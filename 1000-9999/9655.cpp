bc7c269dcd09872a2b2b08e7555ed52f8e738e23
s4ng <48652807+s4ng@users.noreply.github.com>
2019-10-04 19:06:36 +0900

    Create 9655.cpp

---
+++ 1000-9999/9655.cpp
@@ -0,0 +1,12 @@
+//돌 게임
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	cout << (n % 2 == 0 ? "CY" : "SK") << '\n';
+}

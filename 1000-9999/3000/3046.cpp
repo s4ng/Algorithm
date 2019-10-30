d317850c19c7b910b50560022af28c6004e4e30b
s4ng <48652807+s4ng@users.noreply.github.com>
2019-10-30 22:02:54 +0900

    Create 3046.cpp

---
+++ 1000-9999/3000/3046.cpp
@@ -0,0 +1,11 @@
+// R2
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int r1, s;
+	cin >> r1 >> s;
+	cout << 2 * s - r1 << '\n';
+}

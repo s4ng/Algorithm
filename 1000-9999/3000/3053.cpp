9b9ee23c3b62492ca6b76d259d21c844f9e87e65
s4ng <48652807+s4ng@users.noreply.github.com>
2019-10-31 21:03:13 +0900

    Create 3053.cpp

---
+++ 1000-9999/3000/3053.cpp
@@ -0,0 +1,14 @@
+// 택시 기하학
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	double rad, s1, s2;
+	cin >> rad;
+	cout << fixed;
+	cout.precision(6);
+	cout << rad *rad* M_PI << '\n' << rad*rad * 2 << '\n';
+}

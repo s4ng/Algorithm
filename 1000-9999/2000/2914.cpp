acc1f7f91b08cb2d39828fa13f4ecdeeb10f8794
s4ng <48652807+s4ng@users.noreply.github.com>
2019-12-05 11:42:46 +0900

    Create 2914.cpp

---
+++ 1000-9999/2000/2914.cpp
@@ -0,0 +1,12 @@
+// 저작권
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int a, b;
+    cin >> a >> b;
+    cout << a * (b-1) + 1 << '\n';
+}

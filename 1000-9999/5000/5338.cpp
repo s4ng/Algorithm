2b45ed9bc6742f5d376997bffe778ddf16362f52
s4ng <48652807+s4ng@users.noreply.github.com>
2019-12-17 13:27:38 +0900

    Create 5338.cpp

---
+++ 1000-9999/5000/5338.cpp
@@ -0,0 +1,14 @@
+// 마이크로소프트 로고
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cout << "       _.-;;-._" << '\n';
+    cout << "'-..-'|   ||   |" << '\n';
+    cout << "'-..-'|_.-;;-._|" << '\n';
+    cout << "'-..-'|   ||   |" << '\n';
+    cout << "'-..-'|_.-''-._|" << '\n';
+}

abcf8f2ea8b91a240b9e286b79c3afbb37e2ea78
Lee Sang-geun <48652807+s4ng@users.noreply.github.com>
2021-06-13 16:25:34 +0900

    Create 1922.java

---
+++ 1000-9999/1000/1922.java
@@ -0,0 +1,84 @@
+// 네트워크 연결
+import java.util.*;
+
+class Edge {
+    int cost;
+    int a;
+    int b;
+
+    public Edge(int a, int b, int cost) {
+        this.a = a;
+        this.b = b;
+        this.cost = cost;
+    }
+}
+
+public class Main {
+    static int parent[];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        List<Edge> edges = new ArrayList<>();
+
+        for(int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int cost = sc.nextInt();
+
+            edges.add(new Edge(a, b, cost));
+        }
+
+        edges.sort(new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return Integer.compare(o1.cost, o2.cost);
+            }
+        });
+
+        parent = new int[n + 1];
+
+        for(int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        int answer = 0;
+        for(int i = 0; i < edges.size(); i++) {
+            Edge tmp = edges.get(i);
+            if(!isSameParent(tmp.a, tmp.b)) {
+                union(tmp.a, tmp.b);
+                answer += tmp.cost;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static int find(int a) {
+        if(parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a != b) {
+            parent[b] = a;
+        }
+    }
+
+    static boolean isSameParent(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if(a == b) {
+            return true;
+        }
+        return false;
+    }
+}
